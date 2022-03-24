523ec9f16bd830acd304ba5653b984c9de1326b3
21bca33 <96609439+21bca33@users.noreply.github.com>
2022-03-24 11:23:12 +0530

    Add files via upload
    error

---
+++ String/solution16.py
@@ -0,0 +1,23 @@
+# was to enter 5 string in a list. and check and print who's lengh has even num.char without any string function.
+
+def createlist():
+    l=[]
+    for i in range(5):
+        s=input("Enter string:")
+        l.append(s)
+    return(l)
+a=createlist()
+for i in a:
+    print(i)
+
+def evenslengh():
+     count=0
+     for i in k :
+         if k%2==0:
+             count +=i
+     return(k)
+s=evenslengh()    
+print(" lengh of {} is {}".format (count))
+         
+         
+     
